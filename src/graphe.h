64ef58ca76b05e1d941a0d0023cadd358f6fadba
Lucas <lucas.faustmann@gmail.com>
2022-11-30 14:25:34 +0100

    pseudo code classe graphe

---
+++ src/graphe.h
@@ -0,0 +1,34 @@
+#ifndef _GRAPHE_H
+#define _GRAPHE_H
+
+//! sommet (i, j)
+//! indice = i x nbColonne + j
+class graphe
+{
+    public :
+
+    /*
+        constructeur (nbLigne, nbColonne, altitude)
+        int indice (i, j)
+        int altitude (indice)
+        int nord(induce)
+        int sud (indice)
+        intest (indice)
+        int ouest (indice)
+        int voisin (indice, numéroDuVoisin)
+        int nbVoisin (indice)
+        int voisinExiste? (indice, k)
+    */
+
+    private : 
+};
+
+
+
+
+
+
+
+
+
+#endif
