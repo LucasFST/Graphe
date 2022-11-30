fe673894da8961d22e2a6ff09eabfd6a349a7242
Lucas <lucas.faustmann@gmail.com>
2022-11-30 15:16:03 +0100

    classe graphe premieres fonctions et procedures

--- src/graphe.cpp
+++ src/graphe.cpp
@@ -1,1 +1,58 @@
 #include "graphe.h"
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+Graphe::Graphe(unsigned int nombreLignes, unsigned int nombreColonnes)
+{
+    nbLignes = nombreLignes;
+    nbColonnes = nombreColonnes;
+    tableauAltitude = new float [nbLignes * nbColonnes];
+}
+
+Graphe::~Graphe()
+{
+    if(tableauAltitude != NULL)
+    {
+        delete tableauAltitude;
+        tableauAltitude = NULL;
+        nbLignes = 0;
+        nbColonnes = 0;
+    }
+}
+
+float Graphe::getAltitude (unsigned int numeroLigne, unsigned int numeroColonne) const
+{
+    assert(numeroLigne < nbLignes);
+    assert(numeroColonne < nbColonnes);
+    return tableauAltitude [numeroLigne * nbColonnes + numeroColonne];
+}
+
+unsigned int Graphe::getIndice (unsigned int numeroLigne, unsigned int numeroColonne) const
+{
+    assert(numeroLigne < nbLignes);
+    assert(numeroColonne < nbColonnes);
+    return (numeroLigne * nbColonnes + numeroColonne);
+}
+
+unsigned int Graphe::getLigne (unsigned int indice) const
+{
+    assert(indice < nbLignes * nbColonnes);
+    return indice/nbColonnes;
+}
+
+unsigned int Graphe::getColonne (unsigned int indice) const
+{
+    assert(indice < nbLignes * nbColonnes);
+    return indice%nbColonnes;
+}
+
+void Graphe::affichageGraphe() const
+{
+    for(unsigned int i = 0; i < nbColonnes * nbLignes; i++)
+    {
+        if(i % nbColonnes == 0) cout<<endl;
+        cout<<"  "<<tableauAltitude[i]<<"  ";
+    }
+}
--- src/graphe.h
+++ src/graphe.h
@@ -7,26 +7,39 @@
 //! sud(i) = i + nbColonne
 //! est(i) = i + 1
 //! ouest(i) = i - 1
-class graphe
+class Graphe
 {
     public :
+        
+        Graphe (unsigned int nombreLignes, unsigned int nombreColonnes);
+        ~Graphe ();
+        float getAltitude (unsigned int numeroLigne, unsigned int numeroColonne) const;
+        unsigned int getIndice (unsigned int numeroLigne, unsigned int numeroColonne) const;
+        unsigned int getLigne (unsigned int indice) const;
+        unsigned int getColonne (unsigned int indice) const;
+        void affichageGraphe() const;
 
     /*
         constructeur (nbLigne, nbColonne, altitude)
+        destructeur
         int indice (i, j)
         int altitude (indice)
-        int nord(induce)
-        int sud (indice)
-        intest (indice)
-        int ouest (indice)
+        int nord(induce) //! attention préconditions : le voisin doit exister
+        int sud (indice) //! attention préconditions : le voisin doit exister
+        int est (indice) //! attention préconditions : le voisin doit exister
+        int ouest (indice) //! attention préconditions : le voisin doit exister
         int voisin (indice, numéroDuVoisin)
         int nbVoisin (indice)
         int voisinExiste? (indice, k)
         int ligne (indice) = i/C (division entière)
         int colonne (indice) = i%C
+        void modifAltitude
+        void affichageGrilleHauteur
     */
 
     private : 
+        unsigned int nbColonnes, nbLignes; 
+        float* tableauAltitude;
 };
 
 
